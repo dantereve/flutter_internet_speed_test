785302004d7044ce4dea72432e66a3980b79cfa2
Mathias OVIEVE <mathias.ovieve.ext@homeserve.fr>
2024-10-01 17:09:50 +0200

    update dependencies and refactor to accomodate new connectivity_plus way to do things

--- lib/src/speed_test_utils.dart
+++ lib/src/speed_test_utils.dart
@@ -3,7 +3,11 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 Future<bool> isInternetAvailable() async {
   final connectivity = Connectivity();
   final connectivityResult = await connectivity.checkConnectivity();
-  return connectivityResult == ConnectivityResult.mobile ||
-      connectivityResult == ConnectivityResult.wifi || 
-      connectivityResult == ConnectivityResult.ethernet;
+  final onlineStatus = [
+    ConnectivityResult.mobile,
+    ConnectivityResult.wifi,
+    ConnectivityResult.ethernet
+  ];
+
+  return onlineStatus.any((item) => connectivityResult.contains(item));
 }
